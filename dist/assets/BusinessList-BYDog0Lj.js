5a53364dc385631d9eb267a6d27f4cb44baff84f
dalil-tounes <zenanis75@hotmail.com>
2026-05-10 15:39:58 +0200

    Updated index.html

--- dist/assets/BusinessList-BYDog0Lj.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./vendor-react-DSaysAo5.js";import{B as i}from"./BusinessDirectory-DVznl453.js";import"./index-DofwfTjX.js";import"./vendor-icons-CHalgHQ9.js";import"./vendor-router-CBWriE7m.js";import"./vendor-supabase-Dn_fvsz-.js";import"./SignatureCard-Dj3ug7Ny.js";import"./subscriptionTiers-DRzIP6kI.js";function a(){return t.jsx(i,{mode:"citizen"})}export{a as default};
