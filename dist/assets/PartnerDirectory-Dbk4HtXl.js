f1aff3a771faaf605359f548712cf940730e646b
dalil-tounes <zenanis75@hotmail.com>
2026-05-14 13:41:25 +0200

    Updated index.html

--- dist/assets/PartnerDirectory-Dbk4HtXl.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./vendor-react-DSaysAo5.js";import{B as t}from"./BusinessDirectory-BiQacvZm.js";import"./index-C-ysw66v.js";import"./vendor-icons-Bo6zjYE3.js";import"./vendor-router-CBWriE7m.js";import"./vendor-supabase-Dn_fvsz-.js";import"./SignatureCard-Dj3ug7Ny.js";import"./subscriptionTiers-DRzIP6kI.js";function u(){return r.jsx(t,{mode:"partner"})}export{u as default};
