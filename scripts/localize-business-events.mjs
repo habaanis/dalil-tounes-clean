d937c5c0e4bcb7fb47833487871e39f2d40b9e0b
dalil-tounes <zenanis75@hotmail.com>
2026-08-24 18:34:43 +0200

    chore: remove temporary BusinessEvents migration script

--- scripts/localize-business-events.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs';
-const p = 'src/pages/BusinessEvents.tsx';
-let s = fs.readFileSync(p, 'utf8');
-s = s.replace('placeholder="{pageT.searchLabel}, une ville, une entreprise..."', 'placeholder={pageT.searchPlaceholder}');
-fs.writeFileSync(p, s);
